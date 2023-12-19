5aaef2d9b7a21dbfda14f3cf9ce5495a54a54a8d
josuenm <direct.josue@gmail.com>
2023-12-18 22:39:58 -0300

    feat: arquivos para usuário autenticação foi criado

---
+++ src/main/java/com/remedies/josue/user/entities/User.java
@@ -0,0 +1,38 @@
+package com.remedies.josue.user.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/remedies/josue/user/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.remedies.josue.user.repositories;
+
+import com.remedies.josue.user.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByEmail(String email);
+}
